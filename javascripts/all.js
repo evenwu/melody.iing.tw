762605994b429200a471c4d0cbd141d94706db12
goooooooogle <kangxilord@gmail.com>
2015-06-12 05:58:44 +0800

    footer update

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,4 +1,4 @@
-if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if(Modernizr.csstransitions && !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 
   var doc = $(document);
   var body = $('body');
@@ -8,29 +8,46 @@ if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navig
 
   $(window).on('scroll', function(){
     var scrolling = doc.scrollTop();
-    var touch_bottom = body.height() - $(window).height();
-    if (scrolling >= touch_bottom) {
+    var touch_bottom = body.height() - $(window).height() - $('.site-info').outerHeight();
+    if ( scrolling >= touch_bottom ) {
       var viewport_width = $(window).width();
       if (viewport_width >= desktop_breakpoint) {
-        var x = -($(window).height() * 0.8) + 'px';
-        page.css('transform', 'translateY('+x+')');
-        body.addClass('is-scaled');
+        push_page();
       }
     } else {
-      body.removeClass('is-scaled');
-      page.css('transform', 'translateY(0)');
+      reset_page();
     }
   });
 
   $(window).resize(function() {
+    var scrolling = doc.scrollTop();
+    var touch_bottom = body.height() - $(window).height() - $('.site-info').outerHeight();
     var viewport_width = $(window).width();
-    if (viewport_width < desktop_breakpoint) {
+    if (viewport_width < desktop_breakpoint ) {
       body.removeClass('scale-content');
-      page.css('transform', 'translateY(0)');
+      reset_page();
+    } else if (viewport_width >= desktop_breakpoint && scrolling < touch_bottom) {
+      body.addClass('scale-content');
+      reset_page();
     } else {
       body.addClass('scale-content');
+      push_page();
+      $("html, body").scrollTop(doc.height());
     }
   });
 
+  function push_page(){
+    page.css('transform', 'translateY('+ (150 - $(window).height() + 'px') +')');
+    body.addClass('is-scaled');
+  }
+  function reset_page(){
+    page.css('transform', 'translateY(0)');
+    body.removeClass('is-scaled');
+  }
+
 }
 
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
