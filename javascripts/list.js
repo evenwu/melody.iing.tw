750b60d2c5bfb858ad02afd6be25a793172606d3
goooooooogle <kangxilord@gmail.com>
2015-06-25 18:43:56 +0800

    temp version

--- javascripts/list.js
+++ javascripts/list.js
@@ -4,7 +4,7 @@ window.list = [];
 
 window.pageNumber = 1;
 
-window.perPage = 10;
+window.perPage = 100;
 
 songFilter = function(filter) {
   $('.song-list').find(".song-string:not(:Contains(" + filter + "))").parents('li').hide();
