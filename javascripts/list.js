d7ed002e4a16a33787c1f8e6993cee6aa72365a0
goooooooogle <kangxilord@gmail.com>
2015-06-26 00:50:04 +0800

    change list per page

--- javascripts/list.js
+++ javascripts/list.js
@@ -6,7 +6,7 @@ window.list = [];
 
 window.pageNumber = 1;
 
-window.perPage = 10;
+window.perPage = 25;
 
 songFilter = function(filter) {
   $('.song-list').find(".song-string:not(:Contains(" + filter + "))").parents('li').hide();
