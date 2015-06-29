c28af95e358dc60f15067dc2608d0b5bc12310be
goooooooogle <kangxilord@gmail.com>
2015-06-29 18:49:07 +0800

    update list max

--- javascripts/list.js
+++ javascripts/list.js
@@ -6,7 +6,7 @@ window.list = [];
 
 window.pageNumber = 1;
 
-window.perPage = 100;
+window.perPage = 150;
 
 songFilter = function(filter) {
   $('.song-list').find(".song-string:not(:Contains(" + filter + "))").parents('li').hide();
